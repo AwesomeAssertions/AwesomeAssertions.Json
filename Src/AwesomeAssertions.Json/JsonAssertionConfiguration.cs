ad8f30c184c2d80c6df5563d05d98fda069cb280
metacoder_FEFF <metacoder.feff@gmail.com>
2026-02-09 15:38:44 +0000

    add JsonAssertionConfiguration + tests

---
+++ Src/AwesomeAssertions.Json/JsonAssertionConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace AwesomeAssertions.Json
+{
+    /// <summary>
+    /// </summary>
+    public static class JsonAssertionConfiguration
+    {
+        private static Func<string, JToken> _parser = JToken.Parse;
+
+        public static Func<string, JToken> ParseFunction
+        {
+            get
+            {
+                return _parser;
+            }
+
+            set
+            {
+                if(value == null)
+                    throw new ArgumentNullException(nameof(ParseFunction), "Parser cannot be null.");
+                _parser = value;
+            }
+        }
+    }
+}
---
+++ Tests/AwesomeAssertions.Json.Specs/JsonParserCustomizationTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using AwesomeAssertions.Equivalency;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+using Xunit.Sdk;
+
+namespace AwesomeAssertions.Json.Specs
+{
+    public sealed class JsonParserCustomizationTests : IDisposable
+    {
+        // restore JsonAssertionConfiguration.ParseFunction
+        // after each test
+        #region setup/teardown
+        private readonly Func<string, JToken> prevParser;
+
+        public JsonParserCustomizationTests()
+        {
+            prevParser = JsonAssertionConfiguration.ParseFunction;
+        }
+
+        public void Dispose()
+        {
+            JsonAssertionConfiguration.ParseFunction = prevParser;
+            GC.SuppressFinalize(this);           
+        }
+        #endregion
+
+        private readonly Action testSubject = () =>
+        {
+            var date = new JValue("2000-01-01T00:00:00Z");
+
+            date.Should().BeEquivalentTo("\"2000-01-01T00:00:00Z\"");
+        };
+
+        [Fact]
+        public void Assertion_of_string_containing_date__with_default_parser__fails()
+        {
+            testSubject.Should()
+                .Throw<Xunit.Sdk.XunitException>(because: "default parser creates a DateTime when meets a date-like string")
+                .Which
+                .Message.Should().Contain("JSON document has a string instead of a date at $.");
+        }
+
+        [Fact]
+        public void Assertion_of_string_containing_date__with_custom_parser__should_succeed()
+        {
+            JsonAssertionConfiguration.ParseFunction = CustomParser;
+
+            testSubject.Should().NotThrow(); 
+        }
+
+        // this is the same as JToken.Parse()
+        // with additional step:
+        // jsonReader.DateParseHandling = DateParseHandling.None;
+        private static JToken CustomParser(string json)
+        {
+            using var jsonReader = new JsonTextReader(new StringReader(json));
+            
+            jsonReader.DateParseHandling = DateParseHandling.None;
+
+            var result = JToken.Load(jsonReader);
+            while (jsonReader.Read())
+            {
+            }
+
+            return result;
+        }
+    }
+}
