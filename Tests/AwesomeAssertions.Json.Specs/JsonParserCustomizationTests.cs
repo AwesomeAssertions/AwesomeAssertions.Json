44f740bab70f4435ff72e87dd26146bc46005c64
metacoder_FEFF <metacoder.feff@gmail.com>
2026-02-10 09:07:05 +0000

    add other tests

--- Tests/AwesomeAssertions.Json.Specs/JsonParserCustomizationTests.cs
+++ Tests/AwesomeAssertions.Json.Specs/JsonParserCustomizationTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using AwesomeAssertions.Equivalency;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Xunit;
@@ -23,49 +22,155 @@ namespace AwesomeAssertions.Json.Specs
         public void Dispose()
         {
             JsonAssertionConfiguration.ParseFunction = prevParser;
-            GC.SuppressFinalize(this);           
+            GC.SuppressFinalize(this);
         }
         #endregion
 
-        private readonly Action testSubject = () =>
+        #region Test statements with results depending on 'ParseFunction'
+        private void TestSubject_BeEquivalentTo()
         {
             var date = new JValue("2000-01-01T00:00:00Z");
 
             date.Should().BeEquivalentTo("\"2000-01-01T00:00:00Z\"");
-        };
+        }
 
-        [Fact]
-        public void Assertion_of_string_containing_date__with_default_parser__fails()
+        private void TestSubject_NotBeEquivalentTo()
         {
+            var date = new JValue("2000-01-01T00:00:00Z");
+
+            date.Should().NotBeEquivalentTo("\"2000-01-01T00:00:00Z\"");
+        }
+
+        private void TestSubject_ContainSubtree()
+        {
+            var date = new JValue("2000-01-01T00:00:00Z");
+
+            date.Should().ContainSubtree("\"2000-01-01T00:00:00Z\"");
+        }
+
+        private void TestSubject_BeValidJson()
+        {
+            var str = @"
+            {
+                repeating_key: {},
+                repeating_key: {}
+            }";
+
+            str.Should().BeValidJson();
+        }
+        #endregion
+
+        // example of insufficient default behavior
+        [Fact]
+        public void BeEquivalentTo_of_string_containing_date__with_DEFAULT_parser__fails()
+        {
+            // assert with no action (default behavior)
+            Action testSubject = TestSubject_BeEquivalentTo;
+
             testSubject.Should()
-                .Throw<Xunit.Sdk.XunitException>(because: "default parser creates a DateTime when meets a date-like string")
+                .Throw<XunitException>(because: "default parser creates a DateTime when meets a date-like string")
                 .Which
                 .Message.Should().Contain("JSON document has a string instead of a date at $.");
         }
 
         [Fact]
-        public void Assertion_of_string_containing_date__with_custom_parser__should_succeed()
+        public void BeEquivalentTo_of_string_containing_date__with_custom_parser__should_succeed()
         {
+            // act
             JsonAssertionConfiguration.ParseFunction = CustomParser;
 
-            testSubject.Should().NotThrow(); 
+            // assert
+            Action testSubject = TestSubject_BeEquivalentTo;
+            testSubject.Should().NotThrow();
         }
 
-        // this is the same as JToken.Parse()
-        // with additional step:
-        // jsonReader.DateParseHandling = DateParseHandling.None;
+        [Fact]
+        public void ContainSubtree_of_string_containing_date__with_custom_parser__should_succeed()
+        {
+            // act
+            JsonAssertionConfiguration.ParseFunction = CustomParser;
+
+            // assert
+            Action testSubject = TestSubject_ContainSubtree;
+            testSubject.Should().NotThrow();
+        }
+
+        // example of insufficient default behavior
+        [Fact]
+        public void NotBeEquivalentTo_of_string_containing_date__with_DEFAULT_parser__is_false_positive()
+        {
+            // assert with no action (default behavior)
+            Action testSubject = TestSubject_NotBeEquivalentTo;
+
+            testSubject.Should().NotThrow();
+        }
+
+        [Fact]
+        public void NotBeEquivalentTo_of_string_containing_date__with_custom_parser__should_fail()
+        {
+            // act
+            JsonAssertionConfiguration.ParseFunction = CustomParser;
+
+            // assert
+            Action testSubject = TestSubject_NotBeEquivalentTo;
+            testSubject.Should()
+                .Throw<XunitException>()
+                .Which
+                .Message.Should().Contain("Expected JSON document not to be equivalent to");
+        }
+
+        // example of insufficient default behavior
+        [Fact]
+        public void BeValidJson_of_string_containing_repeating_keys__with_DEFAULT_parser__is_false_positive()
+        {
+            // assert with no action (default behavior)
+            Action testSubject = TestSubject_BeValidJson;
+
+            testSubject.Should().NotThrow();
+        }
+
+        [Fact]
+        public void BeValidJson_of_string_containing_repeating_keys__with_custom_parser__should_fail()
+        {
+            // act
+            JsonAssertionConfiguration.ParseFunction = CustomParser;
+
+            // assert
+            Action testSubject = TestSubject_BeValidJson;
+            testSubject.Should()
+                .Throw<XunitException>()
+                .Which
+                .Message.Should().Contain("the name 'repeating_key' already exists");
+        }
+
         private static JToken CustomParser(string json)
         {
             using var jsonReader = new JsonTextReader(new StringReader(json));
-            
-            jsonReader.DateParseHandling = DateParseHandling.None;
 
-            var result = JToken.Load(jsonReader);
+            jsonReader.DateParseHandling = DateParseHandling.None;
+            var settings = new JsonLoadSettings
+            {
+                DuplicatePropertyNameHandling = DuplicatePropertyNameHandling.Error,
+                LineInfoHandling = LineInfoHandling.Ignore,
+                CommentHandling = CommentHandling.Ignore
+            };
+
+            var result = JToken.Load(jsonReader, settings);
             while (jsonReader.Read())
             {
             }
 
             return result;
+        }
+
+        [Fact]
+        public void Setting_null_ParseFunction__should_throw()
+        {
+            Action action = () => JsonAssertionConfiguration.ParseFunction = null!;
+
+            action.Should()
+                .Throw<ArgumentNullException>()
+                .WithMessage("Parser cannot be null. (Parameter 'ParseFunction')");
         }
     }
 }
